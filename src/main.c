01b25affe805570fc6c7e0637eff6aa17101dcda
naouissanayima-sudo <naouissanayima@example.com>
2026-07-09 23:40:46 +0600

    UI: Initialize Raylib 1280x720 window with dark navy background

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include "raylib.h"
+
+int main() {
+    InitWindow(1280, 720, "Polyglot Quest");
+    SetTargetFPS(60); 
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground((Color){13, 27, 42, 255}); 
+        
+        EndDrawing();
+    }
+    CloseWindow(); 
+    return 0;
+}
