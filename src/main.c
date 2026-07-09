78e2d8badd6b3108a9ac7a2897a40d5286588eb1
naouissanayima-sudo <naouissanayima@example.com>
2026-07-10 01:52:49 +0600

    UI: Linked Saïda's screen state machine and main template

--- src/main.c
+++ src/main.c
@@ -1,14 +1,16 @@
 #include "raylib.h"
+#include "types.h"
+#include "logic.h"
 
 int main() {
     InitWindow(1280, 720, "Polyglot Quest");
-    SetTargetFPS(60); 
+    SetTargetFPS(60);
+
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground((Color){13, 27, 42, 255}); 
-        
+        ClearBackground((Color){13, 27, 42, 255});
         EndDrawing();
     }
-    CloseWindow(); 
+    CloseWindow();
     return 0;
 }
---
+++ src/types.h
@@ -0,0 +1,39 @@
+// src/types.h
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef enum {
+    MENU_SCREEN,
+    FLASHCARD_SCREEN,
+    SCENARIO_SCREEN,
+    PROGRESS_SCREEN
+} Screen;
+
+typedef struct {
+    char arabic[100];
+    char english[100];
+    char transliteration[100];
+    char category[50];
+    int masteryLevel;
+} Flashcard;
+
+typedef struct {
+    char prompt[200];
+    char transliteration[200];
+    char translation[200];
+    char options[3][100];
+    int correctIndex;
+    int scoreValue;
+} Scenario;
+
+typedef struct {
+    Screen currentScreen;
+    int totalScore;
+    int correctCount;
+    int wrongCount;
+    int currentCardIndex;
+    int currentScenarioStep;
+    int isRevealed;
+} GameState;
+
+#endif
