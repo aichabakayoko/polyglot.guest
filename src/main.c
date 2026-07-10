74d8c76bf3c89f3da350ba42cd23376d8da36d0a
naouissanayima-sudo <naouissanayima@example.com>
2026-07-10 19:41:49 +0600

    Added path for raylib

--- src/main.c
+++ src/main.c
@@ -2,13 +2,36 @@
 #include "types.h"
 #include "logic.h"
 
+extern GameState state;
+
+void drawMenu() {}
+void drawFlashcard() {}
+void drawScenario() {}
+void drawProgress() {}
+
 int main() {
     InitWindow(1280, 720, "Polyglot Quest");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground((Color){13, 27, 42, 255});
+        ClearBackground((Color){13, 27, 42, 255}); 
+
+        switch (state.currentScreen) {
+            case MENU_SCREEN:
+                drawMenu(); 
+                break;
+            case FLASHCARD_SCREEN:
+                drawFlashcard();
+                break;
+            case SCENARIO_SCREEN:
+                drawScenario(); 
+                break;
+            case PROGRESS_SCREEN:
+                drawProgress(); 
+                break;
+        }
+
         EndDrawing();
     }
     CloseWindow();
