ca3f0185a61e6013ad1288234f04d7228df6af61
naouissanayima-sudo <naouissanayima@example.com>
2026-09-12 10:27:00 +0600

    fix(build): set raylib include and library paths for winlibs environment

--- make.ps1
+++ make.ps1
@@ -3,10 +3,12 @@ If (!(Test-Path -Path "build")) {
     New-Item -ItemType Directory -Path "build" | Out-Null
 }
 
-$RAYLIB_DIR = "C:/Users/nayin/Downloads/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64ucrt-13.0.0-r6/mingw64/x86_64-w64-mingw32"
+# Raylib paths in your WinLibs compiler directory
+$INC = "C:/Users/nayin/Downloads/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64ucrt-13.0.0-r6/mingw64/x86_64-w64-mingw32/include"
+$LIB = "C:/Users/nayin/Downloads/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64ucrt-13.0.0-r6/mingw64/x86_64-w64-mingw32/lib"
 
-# Compile using your exact local configuration
+# Compile using exact local Raylib paths
 gcc src/main.c src/render.c src/ui_menu.c src/ui_flashcards.c src/ui_scenarios.c src/ui_progress.c src/data.c src/logic.c `
-  -I"$RAYLIB_DIR/include" `
-  -L"$RAYLIB_DIR/lib" `
+  -I"$INC" `
+  -L"$LIB" `
   -o build/game.exe -lraylib -lopengl32 -lgdi32 -lwinmm
