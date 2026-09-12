875435752d71468fd98e6a7e89ee3f4bd5454293
naouissanayima-sudo <naouissanayima@example.com>
2026-09-12 10:24:14 +0600

    fix(build): update raylib include and lib paths to x86_64-w64-mingw32 directory

--- make.ps1
+++ make.ps1
@@ -3,8 +3,10 @@ If (!(Test-Path -Path "build")) {
     New-Item -ItemType Directory -Path "build" | Out-Null
 }
 
+$RAYLIB_DIR = "C:/Users/nayin/Downloads/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64ucrt-13.0.0-r6/mingw64/x86_64-w64-mingw32"
+
 # Compile using your exact local configuration
 gcc src/main.c src/render.c src/ui_menu.c src/ui_flashcards.c src/ui_scenarios.c src/ui_progress.c src/data.c src/logic.c `
-  -I"C:/Users/nayin/Downloads/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64ucrt-13.0.0-r6/mingw64/include" `
-  -L"C:/Users/nayin/Downloads/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64ucrt-13.0.0-r6/mingw64/lib" `
+  -I"$RAYLIB_DIR/include" `
+  -L"$RAYLIB_DIR/lib" `
   -o build/game.exe -lraylib -lopengl32 -lgdi32 -lwinmm
